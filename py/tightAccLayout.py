e87147163b712d0e50a0afebbabe2626d22f131d
Braedy Kuzma <braedy@ualberta.ca>
2021-07-25 23:04:12 -0600

    Add initial discussion of layout variations.

--- py/tightAccLayout.py
+++ py/tightAccLayout.py
@@ -1,18 +1,8 @@
-import matplotlib
-
-matplotlib.use("pgf")
-matplotlib.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    'text.usetex': True,
-    'pgf.rcfonts': False,
-})
-
 from matplotlib import pyplot as plt
 
 import thesUtils as tu
 
-logNameFormat = '{type}.64x32x64.v{v}h{h}.CRR.O3.bench.{c}.json'
+logNameFormat = '{type}.{ve}x32x{he}.v{v}h{h}.CRR.O3.bench.{c}.json'
 
 layouts = [
   ((2, 4), '2x4'),
@@ -31,9 +21,9 @@ if __name__ == '__main__':
     # Get the data for each ACC layout.
     for (v, h), layoutName in layouts:
       # Read logs for data.
-      log = logNameFormat.format(type=type, v=v, h=h, c='{}')
+      log = logNameFormat.format(type=type, v=v, h=h, ve=v * 4, he=h * 4, c='{}')
 
-      # Read logs for data.
+      # Save layout data.
       _, _, cycleStats = tu.getJsonCumulativeStats('logs/all/' + log, 25)
       accData[layoutName] = cycleStats
 
@@ -68,14 +58,14 @@ if __name__ == '__main__':
     # Get axis and start plotting.
     ax = fig.add_subplot(figHeight, figWidth, i + 1)
     tu.plotGroupedData(ax, groupLabels, barLabels, bars, errs)
-    ax.set_title(f'\\texttt{{{type}}}')
+    ax.set_title(type.capitalize())
 
     ax.set_xlabel('Accumulator Layout')
     ax.set_ylabel('Cycles')
 
   # Save figure.
-  fig.suptitle('Runtime for Different ACC Layouts per Type ')
+  fig.suptitle('Cycle Counts for Different Accumulator Layouts per Type')
   fig.set_size_inches(tu.textWidth, 5.5)
   fig.tight_layout()
-  fig.savefig('accLayouts.png')
-  fig.savefig('accLayouts.pgf')
+  fig.savefig('tightAccLayouts.png')
+  fig.savefig('tightAccLayouts.pgf')
